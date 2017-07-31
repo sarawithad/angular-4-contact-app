bc3706b867765abac62626116231b3c74871450f
Dara Thomas <darathomas@Daras-Air.wp.comcast.net>
2017-07-31 14:52:36 -0500

    component structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/src/app/contact/contact/contact.component';
+import { AddressComponent } from './address/src/app/address/address/address.component';
+import { DisplayComponent } from './display/src/app/display/display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AddressComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
